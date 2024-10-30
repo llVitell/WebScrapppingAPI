df18d31ffa62a208653e6e6344d883fdb9e0992d
Omar Vite <omar.vite.a@uni.pe>
2024-10-30 14:23:11 -0500

    feat: new querys

--- app.py
+++ app.py
@@ -6,28 +6,29 @@ app = Flask(__name__)
 
 @app.route('/products/<item>', methods=['GET'])
 def get_products(item):
-    limit = request.args.get('limit', default=10, type=int)
+    limit = request.args.get('limit', default=50, type=int)
     page = request.args.get('page', default=1, type=int)
-    
-    url = f'https://listado.mercadolibre.com.pe/{item}?Desde={(page-1)*50+1}&NoIndex_True'
-    response = requests.get(url + '/' + item)
 
+    url = f'https://listado.mercadolibre.com.pe/{item}_Desde_{(page-1)*50+1}_NoIndex_True'
+    response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
+   
     products = soup.find_all('div', class_='poly-card')[:limit]
     products_list = []
 
-    for item in products:
-        title = item.find('h2', class_='poly-component__title').text
-        price = item.find('div', class_='poly-price__current').text
-        url = item.find('h2', class_='poly-component__title').find('a').get('href')
-        img = item.find('img', class_='poly-component__picture').get('src')
+    for product in products:
+        title = product.find('h2', class_='poly-component__title').text if product.find('h2', class_='poly-component__title') else "Sin título"
+        price = product.find('div', class_='poly-price__current').text if product.find('div', class_='poly-price__current') else "Sin precio"
+        url = product.find('h2', class_='poly-component__title').find('a').get('href') if product.find('h2', class_='poly-component__title') and product.find('h2', class_='poly-component__title').find('a') else "Sin URL"
+        img = product.find('img', class_='poly-component__picture').get('src') if product.find('img', class_='poly-component__picture') else "Sin imagen"
+        
         products_list.append({
             'title': title,
             'price': price,
             'url': url,
             'img': img
         })
-
+    
     return jsonify(products_list)
 
 if __name__ == '__main__':
